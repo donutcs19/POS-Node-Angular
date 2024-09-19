3d4ab4168aa0bf2dbee8c2782b5142602f7c67fe
ShiKikie <toon1236@gmail.com>
2024-09-19 13:32:00 +0700

    SaleTemp

--- API/controllers/SaleTempController.js
+++ API/controllers/SaleTempController.js
@@ -1,11 +1,10 @@
 const { PrismaClient } = require('@prisma/client');
-const { create } = require('domain');
 const prisma = new PrismaClient();
 
 module.exports = {
     create: async (req, res) => {
         try{
-        const row = await prisma.food.findMany({
+        const row = await prisma.food.findFirst({
             where: {
                 id: req.body.foodId,
             },
